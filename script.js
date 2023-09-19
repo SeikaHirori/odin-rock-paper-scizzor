e8d3e67a91a71ed06ec8241b4883a03485872586
SeikaHirori <SeikaHirori@outlook.com>
2023-09-19 07:04:52 -0700

    Wrote Rock paper scissor class and its logic

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
 "use strict";
-console.log("Hello world");
+class RockPaperScissor {
+    constructor() {
+        this.currentRound = 0;
+        this.playerOneWins = 0;
+        this.computerWins = 0;
+        this.rpsChoices = ["rock", "paper", "scizzor"];
+    }
+    getComputerChoice() {
+        const randomChoice = Math.floor(Math.random() * this.rpsChoices.length);
+        return this.rpsChoices[randomChoice].toLowerCase();
+    }
+    playRound(playerSelection, computerSelection) {
+        let message = `Something went wrong :3`;
+        switch (playerSelection.toLowerCase()) {
+            case "rock":
+                switch (computerSelection.toLowerCase()) {
+                    case "rock":
+                        message = "Tie!";
+                        break;
+                    case "paper":
+                        message = "Computer wins!";
+                        this.computerWins += 1;
+                        break;
+                    case "scissor":
+                        message = "Player wins!";
+                        this.playerOneWins += 1;
+                        break;
+                    default:
+                        console.log();
+                }
+            case "paper":
+                switch (computerSelection.toLowerCase()) {
+                    case "rock":
+                        message = "Player wins!";
+                        this.playerOneWins += 1;
+                        break;
+                    case "paper":
+                        message = "Tie!";
+                        break;
+                    case "scissor":
+                        message = "Computer wins!";
+                        this.computerWins += 1;
+                        break;
+                    default:
+                        console.log();
+                }
+            case "scissor":
+                switch (computerSelection.toLowerCase()) {
+                    case "rock":
+                        message = "Computer wins!";
+                        break;
+                    case "paper":
+                        message = "Player wins!";
+                        break;
+                    case "scissor":
+                        message = "Tie!";
+                        break;
+                    default:
+                        console.log();
+                }
+            default:
+                console.log(`Something went wrong`);
+                break;
+        }
+        this.currentRound += 1;
+        return message;
+    }
+}
+;
 //# sourceMappingURL=script.js.map
